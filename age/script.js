5464ee8f07c0bad0e23f47ed0c46e438a744f948
juspy <74178467+juspy@users.noreply.github.com>
2021-11-26 16:09:37 +0500

    Add files via upload

---
+++ age/script.js
@@ -0,0 +1,24 @@
+
+
+
+
+var userAge = +prompt('Введите свой возраст: ');
+if (age > 0 && userAge <= 18) {
+    alert('Вы ещё молоды. Вам нужно учиться');
+} else if (userAge > 18 && userAge <= 50) {
+    alert('Вам нужно работать');
+} else if (userAge > 50 && userAge <= 59) {
+    alert('Вам скоро на пенсию');
+} else if (userAge > 59 && userAge <= 150) {
+    alert('Вы пенсионер');
+} else {
+    alert('Что-то пошло не так')
+}
+
+
+
+
+
+
+
+
